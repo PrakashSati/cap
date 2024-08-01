87d49ac031e98b4102b2fc7d32aa248042d634bb
PraksahSati2004 <satip3541@gmail.com>
2024-08-01 21:55:18 +0530

    code correction

--- evalvation/index.js
+++ evalvation/index.js
@@ -39,93 +39,93 @@ let showData= (data) => {
 };
 
 
-// // adding event listener on department select tag;
-// department.addEventListener('change',() => {
-//     departmentFilter();
-// });
+// adding event listener on department select tag;
+department.addEventListener('change',() => {
+    departmentFilter();
+});
 
-// // departmentFilter function that filter our data accoring to departments;
-// let departmentFilter = () => {
-//     let value = department.value;
-//     if(value =='hr' || value == 'marketing' || value == 'finance' || value == 'engineering' || value == 'operations') {
-//         let Url =`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10&filterBy=department&filterValue=${value}`;
-//         fetchRequest(Url);
-//     };
-//     count = 1;
-// };
-
-
-
-
-// // adding event listener on gender select tag;
-// gender.addEventListener('change',() => {
-//     genderFilter();
-// });
-
-// // genderFilter function that filter our data accoring to gender;
-// let genderFilter = async () => {
-//     let value = gender.value;
-//     if(value == 'male' || value == 'female' || value == 'others'){
-//         let Url = `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10&filterBy=gender&filterValue=${value}`;
-//         fetchRequest(Url);
-//     }
-//     count = 1;
-// };
+// departmentFilter function that filter our data accoring to departments;
+let departmentFilter = () => {
+    let value = department.value;
+    if(value =='hr' || value == 'marketing' || value == 'finance' || value == 'engineering' || value == 'operations') {
+        let Url =`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10&filterBy=department&filterValue=${value}`;
+        fetchRequest(Url);
+    };
+    count = 1;
+};
 
 
 
 
-// // adding event listener on salary select tag;
-// salary.addEventListener('change',() => {
-//     salaryFilter();
-// })
-// // salaryFilter function that sort our data accoring to ascending and descending order respected to salary;
-// let salaryFilter = async () => {
-//     let value = salary.value;
-//     if(value == 'asc' || value == 'desc') {
-//         let Url =`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10&filterBy=gender&filterValue=female&sort=salary&order=${value}`;
-//         fetchRequest(Url);
-//     }
-//     count = 1;
-// };
+// adding event listener on gender select tag;
+gender.addEventListener('change',() => {
+    genderFilter();
+});
+
+// genderFilter function that filter our data accoring to gender;
+let genderFilter = async () => {
+    let value = gender.value;
+    if(value == 'male' || value == 'female' || value == 'others'){
+        let Url = `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10&filterBy=gender&filterValue=${value}`;
+        fetchRequest(Url);
+    }
+    count = 1;
+};
+
+
+
+
+// adding event listener on salary select tag;
+salary.addEventListener('change',() => {
+    salaryFilter();
+})
+// salaryFilter function that sort our data accoring to ascending and descending order respected to salary;
+let salaryFilter = async () => {
+    let value = salary.value;
+    if(value == 'asc' || value == 'desc') {
+        let Url =`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10&filterBy=gender&filterValue=female&sort=salary&order=${value}`;
+        fetchRequest(Url);
+    }
+    count = 1;
+};
     
-// // adding addEventListener on next btn for changing the page; 
-// nextBtn.addEventListener('click', () => {
-//     nextHandel();
-// });
+// adding addEventListener on next btn for changing the page; 
+nextBtn.addEventListener('click', () => {
+    nextHandel();
+});
 
 
-// // nextHandel function that increase our pages by 1;
-// let nextHandel = () => {
-//     pageCount++;
-//     if(pageCount > 10) {
-//         pageCount = 1;
-//     }
-//     if(count > 100) {
-//         count = 1;
-//     }
-//     let Url = "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10";
-//     fetchRequest(Url);
-// };
+// nextHandel function that increase our pages by 1;
+let nextHandel = () => {
+    pageCount++;
+    if(pageCount > 10) {
+        pageCount = 1;
+    }
+    if(count > 100) {
+        count = 1;
+    }
+    let Url = "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10";
+    fetchRequest(Url);
+};
 
 
 
-// // adding addEventListener on perv btn for changing the page; 
-// pervBtn.addEventListener('click', () => {
-//     pervHandel();
-// });
+// adding addEventListener on perv btn for changing the page; 
+pervBtn.addEventListener('click', () => {
+    pervHandel();
+});
     
     
 
-// // pervHandel function that decrease our pages by 1;
-// let pervHandel = () => {
-//     pageCount--;
-//     if(pageCount < 1) {
-//         pageCount = 10;
-//     }
-//     if(count < 0 || count > 100) {
-//         count = 1;
-//     }
-//     let Url = "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10";
-//     fetchRequest(Url);
-// };
+// pervHandel function that decrease our pages by 1;
+let pervHandel = () => {
+    pageCount--;
+    if(pageCount < 1) {
+        pageCount = 10;
+    }
+    if(count < 0 || count > 100) {
+        count = 1;
+    }
+    let Url = "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pageCount}&limit=10";
+    fetchRequest(Url);
+};
